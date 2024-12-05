cc0fefd371148489ce74e47c5c168dcf14c092f8
Arun Sharma <arun@sharma-home.net>
2024-12-05 12:26:04 -0800

    part2

--- day5/pageorder.py
+++ day5/pageorder.py
@@ -14,6 +14,7 @@ with open("data1.txt") as f:
             line = [int(i) for i in line.strip().split(",")]
             candidates.append(line)
 
+incorrect = []
 sum = 0
 for c in candidates:
     ok = True
@@ -23,4 +24,16 @@ for c in candidates:
                 ok = False
     if ok:
         sum += c[len(c) // 2]
+    else:
+        incorrect.append(c)
 print(sum)
+
+# second part
+sum2 = 0
+for c in incorrect:
+    for i in range(len(c)):
+        for j in range(i + 1, len(c)):
+            if c[i] in graph and c[j] in graph[c[i]]:
+                (c[i], c[j]) = (c[j], c[i])
+    sum2 += c[len(c) // 2]
+print(sum2)
