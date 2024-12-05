22619557ae0791a9557102b623c79d45b91b89f7
Arun Sharma <arun@sharma-home.net>
2024-12-05 12:19:58 -0800

    Works

--- day5/pageorder.py
+++ day5/pageorder.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python3
 
-import numpy as np
-import math
-from toposort import toposort, CircularDependencyError
 from collections import defaultdict
 
 
 graph = defaultdict(lambda: set())
 candidates = []
+left_set = set()
+right_set = set()
 with open("data1.txt") as f:
     for line in f.readlines():
         if "|" in line:
             x, y = [int(v) for v in line.split("|")]
+            left_set.add(x)
+            right_set.add(y)
             graph[y].add(x)
         elif "," in line:
+            line = [int(i) for i in line.strip().split(",")]
             candidates.append(line)
-
-ordering = {}
-last = 0
-try:
-    for i, level in enumerate(toposort(graph)):
-        for page in level:
-            ordering[page] = i
-        last = i
-except CircularDependencyError as e:
-    for k in e.data.keys():
-        ordering[k] = last + 1
-
+graph = dict(graph)
 
 sum = 0
 for c in candidates:
-    c = [int(i) for i in c.strip().split(",")]
-    o = np.array([ordering[i] for i in c])
-    diffs = np.diff(o)
-    ok = np.all(diffs > 0)
+    ok = True
+    for i in range(len(c)):
+        for j in range(i + 1, len(c)):
+            if c[i] in graph and c[j] in graph[c[i]]:
+                ok = False
     if ok:
-        sum += c[math.floor(len(c) / 2)]
+        sum += c[len(c) // 2]
 print(sum)
